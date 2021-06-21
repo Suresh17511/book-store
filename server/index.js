c1ba1e9a025b1d9a08a19bf1d982742026f475a3
Suresh17511 <suresh.chidurala17511@gmail.com>
2021-06-21 08:05:31 +0530

    book-store V.1

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import dotenv from 'dotenv';
 import bookRoutes from './routes/books.js';
 
 const app = express();
+dotenv.config();
 
 app.use(express.json({limit: '30mb', extended: true}));
 app.use(express.urlencoded({limit: '30mb', extended: true}));
@@ -12,12 +13,13 @@ app.use(cors());
 
 app.use('/books', bookRoutes);
 
-const CONNECTION_URL =
-  'mongodb+srv://book_store:ROA0LRPP1hWY6ZNx@cluster0.69m22.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() =>
     app.listen(PORT, () =>
       console.log(`Server Running on Port: http://localhost:${PORT}`)
